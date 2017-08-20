8c1e8b99e09465e013235c10884baf9044c15350
PFinnfors <pier@live.se>
2017-08-20 14:52:27 +0200

    Add project files.

---
+++ BayesApp/Controllers/WeatherController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BayesApp.Controllers
+{
+    public class WeatherController : Controller
+    {
+
+    }
+}
